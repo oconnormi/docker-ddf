4f11cbb824a735db8d572b5989509762218ef822
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2016-10-04 15:04:07 -0400

    Created ddf-base image
    * ddf now depends on oconnormi/ddf-base
    * ddf-base provides entrypoint and java

--- alpine-standard/entrypoint/pre_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "This is a placeholder script, replace this with a customized Pre-Start configuration script as needed by volume mounting a file to ${ENTRYPOINT_HOME}/pre_start.sh"
